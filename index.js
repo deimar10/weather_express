8e5d79d41ca4d543d531cc2643002936c9d43de3
deimar <deimar.parn@khk.ee>
2022-02-01 12:35:42 +0000

    “Created form request responce with API data”

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
+const https = require("https");
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -26,4 +29,23 @@ app.get('/', function (req, res) {
             })
         })
     })
+app.post('/', function(req,res){
+    console.log(req.body)
+   let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
+        city}&appid=${key}`)
+        .then((responce) =>{
+            return responce.json()
+        })
+        .then((data) =>{
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
 app.listen(1800 )
